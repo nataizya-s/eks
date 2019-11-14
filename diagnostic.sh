db1b599fb37f543bcf73b654836bd7cce92342bb
nasika <nasika@u5020d8f65b2156.ant.amazon.com>
2019-11-14 13:21:33 +0200

    initialising git repo

---
+++ diagnostic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir diagnostics
+
+cat <<EOT >> diagnostics/cluster_info
+====Cluster Info====
+
+$(kubectl cluster-info dump) >& /dev/null
+
+EOT
+
+cat <<EOT >> diagnostics/resources
+
+$(kubectl get all --all-namespaces -o wide >& /dev/null) 
+
+EOT
+
+cat <<EOT >> diagnostics/cluster_events
+
+$(kubectl get events >& /dev/null)
+
+EOT
+
+
