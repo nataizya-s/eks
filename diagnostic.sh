e3639bf4a4dfe30263a9b7fb2b17b1f680b87da3
nasika <nasika@u5020d8f65b2156.ant.amazon.com>
2019-11-15 08:36:52 +0200

    added aws-node debugging commands

--- diagnostic.sh
+++ diagnostic.sh
@@ -17,8 +17,24 @@ EOT
 
 cat <<EOT >> diagnostics/cluster_events
 
-$(kubectl get events >& /dev/null)
+$(kubectl get events)
 
 EOT
 
+cat <<EOT >> diagnostics/aws-auth
 
+$(kubectl get cm aws-auth -n kube-system -o yaml)
+
+EOT
+
+cat <<EOT >> diagnostics/aws-node-logs
+
+====CNI Describe Output====
+
+$(kubectl describe pod $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system)
+
+====CNI Pod Logs====
+
+$(kubectl logs $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system)
+
+EOT
