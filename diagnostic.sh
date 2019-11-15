2b6930bdeb77ef06bca1689aaccae8e48facf9df
nasika <nasika@u5020d8f65b2156.ant.amazon.com>
2019-11-15 09:04:15 +0200

    adding zipping functionality

--- diagnostic.sh
+++ diagnostic.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mkdir diagnostics
 
 cat <<EOT >> diagnostics/cluster_info
 ====Cluster Info====
 
-$(kubectl cluster-info dump) >& /dev/null
+$(kubectl cluster-info dump)
 
 EOT
 
 cat <<EOT >> diagnostics/resources
 
-$(kubectl get all --all-namespaces -o wide >& /dev/null) 
+$(kubectl get all --all-namespaces -o wide)
 
 EOT
 
 cat <<EOT >> diagnostics/cluster_events
-
 $(kubectl get events)
 
 EOT
@@ -31,10 +30,13 @@ cat <<EOT >> diagnostics/aws-node-logs
 
 ====CNI Describe Output====
 
-$(kubectl describe pod $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system)
+$(kubectl describe pod $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system) 
 
 ====CNI Pod Logs====
 
-$(kubectl logs $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system)
+$(kubectl logs $(kubectl get pods -n kube-system -o=name | grep aws-node | sed "s/^.\{4\}//") -n kube-system) 
 
 EOT
+
+#zip file 
+tar --create --verbose --gzip --file diagnostics_$(date --utc +%Y-%m-%d_%H%M-%Z).tar.gz --directory=diagnostics/ . > /dev/null 2>&1
